5b779c23877b6f74c0adda2244440d8563fae56e
ayeshafarheena <77048917+ayeshafarheena@users.noreply.github.com>
2026-05-16 14:04:06 +0530

    Change EC2 instance type from t2.micro to t2.medium

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "myec2" {
   ami           = "ami-0c02fb55956c7d316"
-  instance_type = "t2.micro"
+  instance_type = "t2.medium"
 
   tags = {
     Name = "MyServer"
